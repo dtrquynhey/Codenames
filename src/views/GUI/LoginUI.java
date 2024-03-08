1585a611a26a2bb48a3347dab25bdf34fe80f6e6
dtrquynhey <dtrquynhey@gmail.com>
2024-03-08 13:32:10 -0500

    Add textFieldUsername

--- src/views/GUI/LoginUI.java
+++ src/views/GUI/LoginUI.java
@@ -2,6 +2,7 @@ package views.GUI;
 
 import views.customPalettes.Frame;
 import views.customPalettes.ShadowLabel;
+import views.customPalettes.TextField;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,9 +20,18 @@ public class LoginUI extends Frame {
         gridBagConstraints.insets = new Insets(0, 0, 10, 0);
         panel.add(lblTitle, gridBagConstraints);
 
+        JTextField textFieldUsername = new TextField("Username", new Dimension(200, 42));
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.insets = new Insets(5, 0, 0, 0);
+        panel.add(textFieldUsername, gridBagConstraints);
+
+        setVisible(true);
+
     }
 
     public static void main(String[] args) {
+
         new LoginUI();
     }
 }
