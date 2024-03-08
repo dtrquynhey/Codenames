bc3b7dd3e62b35da6e5faaf134d3bd605c7fbef7
dtrquynhey <dtrquynhey@gmail.com>
2024-03-08 11:57:44 -0500

    Fix button's color

--- src/views/GUI/WelcomeUI.java
+++ src/views/GUI/WelcomeUI.java
@@ -16,25 +16,32 @@ public class WelcomeUI extends Frame {
 
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
 
-        JLabel lblTitle = new ShadowLabel("CODENAMES", 87, Color.WHITE);
+        JLabel lblTitle = new ShadowLabel("CODENAMES", 96, Color.WHITE);
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.insets = new Insets(0, 0, 10, 0);
         panel.add(lblTitle, gridBagConstraints);
 
         RoundedButton btnNewGame = new RoundedButton("New Game");
-        btnNewGame.setBackground(Color.decode("#88E0EF"));
+        btnNewGame.setBackground(Color.decode("#E5D1D2"));
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
-        gridBagConstraints.insets = new Insets(0, 0, 4, 0);
+        gridBagConstraints.insets = new Insets(5, 0, 0, 0);
         panel.add(btnNewGame, gridBagConstraints);
 
         RoundedButton btnReadRules = new RoundedButton("Read Rules");
-        btnReadRules.setBackground(Color.decode("#FF5151"));
+        btnReadRules.setBackground(Color.decode("#E4C988"));
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 2;
-        gridBagConstraints.insets = new Insets(4, 0, 0, 0);
+        gridBagConstraints.insets = new Insets(5, 0, 0, 0);
         panel.add(btnReadRules, gridBagConstraints);
+
+        RoundedButton btnLogOut = new RoundedButton("Log Out");
+        btnLogOut.setBackground(Color.decode("#FC9355"));
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.insets = new Insets(5, 0, 0, 0);
+        panel.add(btnLogOut, gridBagConstraints);
 
         btnReadRules.addActionListener(e -> {
             // Open RuleUI when the button is clicked
