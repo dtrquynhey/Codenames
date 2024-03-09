38661994d322b3f350f9685ac846a7243bc4cf1f
dtrquynhey <dtrquynhey@gmail.com>
2024-03-09 01:12:01 -0500

    Fix Label

--- src/views/GUI/LoginUI.java
+++ src/views/GUI/LoginUI.java
@@ -2,14 +2,13 @@ package views.GUI;
 
 import views.customPalettes.*;
 import views.customPalettes.Frame;
+import views.customPalettes.Label;
 import views.customPalettes.TextField;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class LoginUI extends Frame {
-
-    private JPanel panel1;
 
     public LoginUI() {
         super(new GridBagLayout());
@@ -34,16 +33,17 @@ public class LoginUI extends Frame {
         gridBagConstraints.insets = new Insets(5, 0, 0, 0);
         panel.add(passwordField, gridBagConstraints);
 
-        FlowLayout buttonLayout = new FlowLayout();
-        Panel buttonPanel = new Panel(buttonLayout);
-        JButton buttonLogIn = new RoundedButton("Log In", 110, 42, Color.decode("#E5D1D2"));
-        JButton buttonSignUp = new RoundedButton("Sign Up", 110, 42, Color.decode("#E4C988"));
-        buttonPanel.add(buttonLogIn);
-        buttonPanel.add(buttonSignUp);
+        JButton buttonLogIn = new RoundedButton("Log In", 110, 42, Color.decode("#FC9355"));
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 3;
         gridBagConstraints.insets = new Insets(15, 0, 0, 0);
-        panel.add(buttonPanel, gridBagConstraints);
+        panel.add(buttonLogIn, gridBagConstraints);
+
+        JLabel labelSignUp = new Label("Don't have an account yet?", Font.PLAIN, 20, Color.WHITE);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 8;
+        gridBagConstraints.insets = new Insets(15, 0, 0, 0);
+        panel.add(labelSignUp, gridBagConstraints);
 
         setVisible(true);
 
