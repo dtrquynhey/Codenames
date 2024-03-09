dc23b502fdd3b28f94eeb83e5c6d5ff09e16bad8
dtrquynhey <dtrquynhey@gmail.com>
2024-03-09 01:53:01 -0500

    Implement TopFlowPanel

---
+++ src/views/customPalettes/TopFlowPanel.java
@@ -0,0 +1,23 @@
+package views.customPalettes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class TopFlowPanel extends JPanel {
+
+    private ArrayList<RoundedButton> buttons;
+    public TopFlowPanel() {
+        buttons = new ArrayList<>();
+        setLayout(new FlowLayout());
+    }
+
+    public void addButton(RoundedButton button) {
+        buttons.add(button);
+        add(button);
+    }
+
+    public ArrayList<RoundedButton> getButtons() {
+        return buttons;
+    }
+}
